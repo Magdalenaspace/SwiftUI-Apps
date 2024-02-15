8d128d37059586a96d26f4db12578d49ff0a1c12
Magdalena Samuel <96504344+Magdalenaspace@users.noreply.github.com>
2024-02-15 12:24:23 -0800

    Add files via upload

---
+++ ZoomPeek/ZoomPeek/Core/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  ZoomPeek
+//
+//  Created by Magdalena Samuel on 1/18/24.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+  Page(id: 1, imageName: "magazine-front-cover"),
+  Page(id: 2, imageName: "magazine-back-cover")
+]
